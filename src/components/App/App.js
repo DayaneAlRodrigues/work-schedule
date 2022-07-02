cd4971535780ba206e4883a6c5f0593e755a697f
Dayane Rodrigues <dayanealrodrigues@gmail.com>
2022-07-02 15:44:01 -0300

    Primeiro commit

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Button from '@material-ui/core/Button';
+import Login from '../login';
+
+function App() {
+  return (
+    <div>
+      <Button>Olá</Button>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/login/login.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <>
+            <h1>Entre com Google ou Facebook</h1>
+            <p> Ou :</p>
+            <p> Login: </p>
+            <input></input>
+            <p>Senha: </p>
+            <input></input>
+            <p>Cadastre aqui: </p>
+            <p> Login: </p>
+            <input></input>
+            <p>Senha: </p>
+
+            </>
+        );
+    }
+}
+ 
+
+export default Login;
